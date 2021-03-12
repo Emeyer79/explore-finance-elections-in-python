e2aaaccba63c87a5704d4f98b1cd32dc73e3f750
emeyer79 <eric.meyer79@gmail.com>
2021-03-12 10:47:00 -0600

    python homework

--- PyBank/pybank.py
+++ PyBank/pybank.py
@@ -1,3 +1,5 @@
+
+
 # Directions-------------------------------------------------------------------
 
 #   * The total number of months included in the dataset
@@ -16,28 +18,31 @@ import csv
 
 # 2: Create the csv path and view columns
 pybnk_csv = os.path.join('budget_data.csv')
-    with open(pybnk_csv) as csvfile:
-        csvreader = csv.reader(csvfile, delimiter = ',')
-        header = next(csvreader)
-        print(f"Header: {header}")
+with open(pybnk_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    header = next(csvreader)
+    print(f"Header: {header}")
 
-        # 3: Create placeholders for each element 
-        date = []
-        profit = []
-        profit_change = []
-                        
-        # 4: Create loop and go through 
-        for row in csvreader:
-            date.append(row[0])
-            profit.append(int(row[1]))
-        for i in range(len(profit)-1):
-            profit_change.append(profit[i+1]-profit[i])
+    # 3: Create placeholders for each element in empty list
+    date = []
+    profit = []
+    profit_change = []
+                     
+    # 4: Create loop and go through 
+    for row in csvreader:
+        date.append(row[0])
+        profit.append(int(row[1]))
+    for i in range(len(profit)-1):
+        profit_change.append(profit[i+1]-profit[i])
+    
                       
 #Create the MAX and MIN add formatting for USD
 max_inc = max(profit_change)
 min_dec = min(profit_change)
 max_inc_f = "${:,.2f}".format(max_inc)
 min_dec_f = "${:,.2f}".format(min_dec)
+print(max_inc_f)
+print(min_dec_f)
 
 #profit formatting and total
 sum_profit = sum(profit)
@@ -48,7 +53,7 @@ avg_prof = round(sum(profit_change)/len(profit_change),2)
 avg_prof_f = "${:,.2f}".format(avg_prof)
 
 
-#Index 
+#Index for month increase and decrease
 month_increase = profit_change.index(max(profit_change))+1
 month_decrease = profit_change.index(min(profit_change))+1
 
